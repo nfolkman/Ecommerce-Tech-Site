28e8580b2a3391190afcc0ea44fbab6391d64f35
nfolkman <nathanielfolkman@gmail.com>
2022-11-15 17:41:06 -0800

    first commit - sanity build-out and DB structure/Hero design

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import { client } from '../lib/client'
+
+import { Product, FooterBanner, HeroBanner } from '../components'
+
+
+const Home = ({ products, bannerData }) => {
+  return (
+    <>
+      <HeroBanner heroBanner= { bannerData.length && bannerData[0] } />
+
+      {console.log(bannerData)}
+
+      <div className='products-heading'>
+        <h2>Best Sellers</h2>
+        <p>Speaker Selection</p>
+      </div>
+
+      <div className='products-container'>
+        {products?.map((product) => product.name)}
+      </div>
+
+
+      <FooterBanner />
+    </>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query)
+
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery)
+
+  return {
+    props:{ products, bannerData }
+  }
+}
+
+export default Home
